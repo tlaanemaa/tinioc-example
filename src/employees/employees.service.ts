731cb4cc96d327c175c90dd4bcc77ac303da15c0
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2021-09-18 16:45:32 +0200

    Added employee service

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,22 @@
+import { declareDependency } from "tinioc";
+import {
+  IEmployeesService,
+  IEmployeesClient,
+  EMPLOYEES_CLIENT,
+} from "../declarations";
+
+export const employeesService = declareDependency(
+  ({ get }): IEmployeesService => ({
+    getYoungest: async () => {
+      const client = get<IEmployeesClient>(EMPLOYEES_CLIENT);
+      const employees = await client.getAll();
+      return employees.sort((a, b) => a.employee_age - b.employee_age)[0];
+    },
+
+    getOldest: async () => {
+      const client = get<IEmployeesClient>(EMPLOYEES_CLIENT);
+      const employees = await client.getAll();
+      return employees.sort((a, b) => b.employee_age - a.employee_age)[0];
+    },
+  })
+);
