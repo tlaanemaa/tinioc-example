54c0ee3629dc22382e988b313fbe2a389389de16
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2021-09-18 16:46:05 +0200

    Added bindings for components

--- src/bindings.ts
+++ src/bindings.ts
@@ -1,7 +1,18 @@
 import { Container } from "tinioc";
-import { INumbersDB, NUMBERS_DB } from "./declarations";
+import {
+  INumbersDB,
+  NUMBERS_DB,
+  IEmployeesClient,
+  EMPLOYEES_CLIENT,
+  IEmployeesService,
+  EMPLOYEES_SERVICE,
+} from "./declarations";
 import database from "./database";
+import { employeesClient } from "./employees/employees.client";
+import { employeesService } from "./employees/employees.service";
 
 export const container = new Container();
 
 container.bind<INumbersDB>(NUMBERS_DB, database);
+container.bind<IEmployeesClient>(EMPLOYEES_CLIENT, employeesClient);
+container.bind<IEmployeesService>(EMPLOYEES_SERVICE, employeesService);
