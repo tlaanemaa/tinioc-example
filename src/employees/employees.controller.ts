cb2a2010f36f8e173095a16041bffc8af6b34b07
Taavi Laanemaa <taavi.laanemaa@klarna.com>
2021-09-19 21:53:37 +0200

    Added error handling to the controller

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -2,18 +2,29 @@ import { Router } from "express";
 import { v4 as uuidv4 } from "uuid";
 import { IEmployeesService, EMPLOYEES_SERVICE } from "./bindings";
 import { IRequestContext, REQUEST_CONTEXT } from "../context";
-import { container } from "./container";
+import { container as employeesContainer } from "./container";
+import { ILogger, LOGGER } from "../logger";
 
 export const employeesRouter = Router();
 
 employeesRouter.get("/oldest", async (req, res, next) => {
   const ctx = { correlationId: req.header("correlation-id") ?? uuidv4() };
-  const result = await container
+  const container = await employeesContainer
     .createChild()
-    .bind<IRequestContext>(REQUEST_CONTEXT, () => ctx)
-    .get<IEmployeesService>(EMPLOYEES_SERVICE)
-    .getOldest();
+    .bind<IRequestContext>(REQUEST_CONTEXT, () => ctx);
 
-  res.json(result);
+  try {
+    const result = await container
+      .get<IEmployeesService>(EMPLOYEES_SERVICE)
+      .getOldest();
+
+    res.json(result);
+  } catch (e) {
+    const message = e instanceof Error ? e.message : "Something broke";
+    const logger = container.get<ILogger>(LOGGER);
+    logger.error(message);
+    res.status(500).json({ error: "Something broke :(" });
+  }
+
   next();
 });
