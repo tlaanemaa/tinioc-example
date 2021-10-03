170ee5b24e8ae6616c377de4157a26057a280476
Taavi Laanemaa <taavi.laanemaa@klarna.com>
2021-10-03 21:03:49 +0200

    Added a fancier startup log

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,15 @@ import routes from "./routes";
 
 const app = express();
 const port = process.env.PORT || 3000;
+const host = `http://localhost:${port}`;
+
 app.use(routes);
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+
+app.listen(port, () =>
+  console.log(
+    [
+      `Listening on ${host}`,
+      `See ${host}/employees/youngest for an example route`,
+    ].join("\n")
+  )
+);
